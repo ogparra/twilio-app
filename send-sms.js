f24cdaa782344584a0ab2254d9f0d73320ffab58
ogparra <ogparra.15@gmail.com>
2018-10-22 14:50:07 -0700

    NodeJS SMS application

---
+++ send-sms.js
@@ -0,0 +1,21 @@
+
+// Read acct_id and auth_tok id from twilio_creds file
+
+require('dotenv').config()
+
+
+const acct_id = process.env.TWILIO_ACCTID;
+const auth_tok = process.env.TWILIO_AUTHTOK;
+
+const twilio = require('twilio');
+
+const client = new twilio(acct_id, auth_tok);
+
+client.messages.create({
+    body: 'Hey Oscar, this message was sent using the Twilio API',
+    to: '+13109560176',  // Text this number
+    from: '+14242908182' // From a valid Twilio number
+})
+.then((message) => console.log(message.sid));
+
+
---
+++ test-proces-env.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+
+console.log("TWILIO_ACCTID: " + process.env.TWILIO_ACCTID);
+console.log("TWILIO_AUTHTOK: " + process.env.TWILIO_AUTHTOK);
